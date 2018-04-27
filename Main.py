2c792f646427dadf406d9f56bf3f4f92039d73da
mattiazingaretti <zingaretti.mattia@gmail.com>
2018-04-27 19:14:22 +0200

    Updates

--- Main.py
+++ Main.py
@@ -8,8 +8,8 @@ cap = cv2.VideoCapture(0)
 #Parametri hsv da cercare
 lower_red = np.array([140,50,0])#da controllare i valori con le dovute proporzioni.
 upper_red = np.array([140,50,0])
-lower_blue = np.array([140,50,0])#da controllare i valori con le dovute proporzioni.
-upper_blue = np.array([140,50,0])
+lower_blue = np.array([97,100,40])#da controllare i valori con le dovute proporzioni.
+upper_blue = np.array([117,255,200])
 lower_green = np.array([140,50,0])#da controllare i valori con le dovute proporzioni.
 upper_green = np.array([140,50,0])
 
@@ -31,22 +31,22 @@ while cap and save:
     foto = cv2.imread( 'frame/0frame.png', cv2.IMREAD_COLOR)#leggo il frame
     hsv = cv2.cvtColor(foto, cv2.COLOR_BGR2HSV)#converto in hsv
 
-    greyRed = cv2.inRange(hsv, lower_red, upper_red)#ottengo la scala di grigi in base al threshold fornito
-    #greyBlue = cv2.inRange(hsv, lower_blue, upper_blue)
+    #greyRed = cv2.inRange(hsv, lower_red, upper_red)#ottengo la scala di grigi in base al threshold fornito
+    greyBlue = cv2.inRange(hsv, lower_blue, upper_blue)
     #greyGreen = cv2.inRange(hsv, lower_green, upper_green)
 
     #Applico hough circle transform
-    circlesRed = cv2.HoughCircles(greyRed, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=30, minRadius=20, maxRadius=200)
+    circlesRed = 0#cv2.HoughCircles(greyRed, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=30, minRadius=20, maxRadius=200)
     circlesGreen = 0#cv2.HoughCircles(greyGreen, cv2.HOUGH_GRADIENT, 1, 50 ,param1 = 50, param2 = 30, minRadius = 20, maxRadius = 200)
-    circlesBlue = 0#cv2.HoughCircles(greyBlue, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=30, minRadius=20, maxRadius=200)
+    circlesBlue = cv2.HoughCircles(greyBlue, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=30, minRadius=20, maxRadius=200)
 
     #Gestisco il caso in cui non vi siano cerchi nel frame( assurdo )
     if circlesRed or circlesGreen or circlesBlue is None:
         print("Nessuna circonferenza trovata")
         continue
     #Disegno le circonferenze DEBUG
-    circlesRed = np.uint16(np.around(circlesRed))
-    for i in circlesRed[0, :]:
+    circlesBlue = np.uint16(np.around(circlesBlue))
+    for i in circlesBlue[0, :]:
         cv2.circle(foto, (i[0], i[1]), i[2], (0, 0, 255), 1)  # draw the outer circle
         cv2.circle(foto, (i[0], i[1]), 2, (0, 0, 255), 3)  # draw the center of the circle
         cv2.imwrite('processed/0processed.png',foto)
