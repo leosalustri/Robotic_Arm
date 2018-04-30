1fd615cb2b1b2406bd9e2cdf389fc08854becde3
mattiazingaretti <zingaretti.mattia@gmail.com>
2018-04-30 17:55:10 +0200

    Updates

--- Main.py
+++ Main.py
@@ -33,13 +33,13 @@ while cap and save:
     foto = cv2.imread( 'frame/0frame.png', cv2.IMREAD_COLOR)#leggo il frame
     hsv = cv2.cvtColor(foto, cv2.COLOR_BGR2HSV)#converto in hsv
 
-    #greyRed = cv2.inRange(hsv, lower_red, upper_red)#ottengo la scala di grigi in base al threshold fornito
+    greyRed = cv2.inRange(hsv, lower_red, upper_red)#ottengo la scala di grigi in base al threshold fornito
     greyBlue = cv2.inRange(hsv, lower_blue, upper_blue)
-    #greyGreen = cv2.inRange(hsv, lower_green, upper_green)
+    greyGreen = cv2.inRange(hsv, lower_green, upper_green)
 
     #Applico hough circle transform settare i parametri in base alle conversioni cm in pixel del sistema di riferimento.
-    circlesRed = 0#cv2.HoughCircles(greyRed, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=30, minRadius=20, maxRadius=200)
-    circlesGreen = 0#cv2.HoughCircles(greyGreen, cv2.HOUGH_GRADIENT, 1, 50 ,param1 = 50, param2 = 30, minRadius = 20, maxRadius = 200)
+    circlesRed = cv2.HoughCircles(greyRed, cv2.HOUGH_GRADIENT, 1, 50, param1=50, param2=30, minRadius=20, maxRadius=200)
+    circlesGreen = cv2.HoughCircles(greyGreen, cv2.HOUGH_GRADIENT, 1, 50 ,param1 = 50, param2 = 30, minRadius = 20, maxRadius = 200)
     circlesBlue = cv2.HoughCircles(greyBlue, cv2.HOUGH_GRADIENT, 1, 300, param1=50, param2=30, minRadius=10, maxRadius=400)
 
     #Gestisco il caso in cui non vi siano cerchi nel frame( assurdo )
@@ -52,3 +52,41 @@ while cap and save:
         cv2.circle(foto, (i[0], i[1]), i[2], (255, 0, 0), 1)  # draw the outer circle
         cv2.circle(foto, (i[0], i[1]), 2, (255, 0, 0), 3)  # draw the center of the circle
         cv2.imwrite('processed/0processed.png',foto)
+
+
+
+
+
+#return angles in radiants
+def inverseCinematic(x, y, z, l1, l2, l3):
+    fi = 30 *math.pi/180 #Angolo che definisce l'orientamento della pinza rispetto all'asse x.
+    xp = x -l3*math.cos(fi)
+    zp = z -l3*math.sin(fi)
+
+    teta2 = math.acos( ((xp*xp + zp*zp -l1*l1-l2*l2)/2*l1*l2))
+    teta1 = math.atan2( (-l2*math.sin(teta2)*xp + ((l1+l2*math.cos(teta2))*zp)),((l1+l2*math.cos(teta2))*xp + l2*math.sin(teta2)*zp ))
+    teta3 = fi - teta1 - teta2
+    teta0 = math.atan2(x,y)
+    angoli = [teta0,teta1,teta2,teta3]
+    return angoli
+
+def inverseCinematic2(xd, yd, zd, betad, d1,l1,l2,l3,d5):
+
+    teta1 = math.atan2(yd, xd)
+
+    A1 = xd*math.cos(teta1) + yd*math.sin(teta1) - d5*math.cos(betad) -l1
+    A2 = d1 - zd - d5*math.sin(betad)
+
+    teta3 = math.acos(((A1*A1 + A2*A2 -l2*l2 -l3*l3)/2*l2*l3))
+    teta2 = math.atan2(((l2 + l3*math.cos(teta3))*A2 - l3*math.sin(teta3)*A1),((l2 + l3*math.cos(teta3))*A1 +l3*math.sin(teta3)*A2))
+    teta4 = betad - teta2 - teta3 - math.pi/2
+
+    angoli = [teta1, teta2, teta3, teta4]
+    return angoli
+
+angoli = inverseCinematic2(0,2,1,0, 0, 0,1,1,1)
+i = 0
+for angolo in angoli:
+    angolo = angolo*180/math.pi
+    i = i +1
+    print("angolo", i, angolo)
